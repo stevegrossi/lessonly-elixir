f0ece228b78cdbb7330896ab90f536c921b90742
Steve Grossi <me@stevegrossi.com>
2016-04-03 16:17:46 -0400

    Update mix.exs for Hex.pm

--- mix.exs
+++ mix.exs
@@ -5,27 +5,29 @@ defmodule Lessonly.Mixfile do
     [app: :lessonly,
      version: "0.0.1",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  defp description do
+    """
+    An Elixir wrapper for the Lesson.ly API.
+    """
+  end
+
+  def package do
+    [
+      maintainers: ["Steve Grossi"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/stevegrossi/lessonly-elixir"}
+    ]
+  end
+
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:httpoison, "~> 0.8.0"},
